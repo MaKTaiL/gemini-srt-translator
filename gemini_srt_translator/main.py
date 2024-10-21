4906b17966b73ad2657a313180f3d33cfdfd8951
Matheus Castro <matheuscastro@gmail.com>
2024-10-21 10:15:59 -0300

    Refactor translation logic and improve error handling; update version to 1.1.4

--- gemini_srt_translator/main.py
+++ gemini_srt_translator/main.py
@@ -46,8 +46,8 @@ class GeminiSRTTranslator:
 
         genai.configure(api_key=self.gemini_api_key)
 
-        instruction = f"""You are an assistant that translates movies/series subtitles to {self.target_language}.
-        User will send a json and you must return a copy of it with the dialogues translated. Return the same indices as the input."""
+        instruction = f"""You are an assistant that translates subtitles to {self.target_language}.
+        You will receive a json and you must return a copy of it with the dialogues translated. Return the same indices as the input."""
 
         model = genai.GenerativeModel(
             model_name=self.model_name,
@@ -58,7 +58,7 @@ class GeminiSRTTranslator:
                 HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
             },
             system_instruction=instruction,
-            generation_config={"response_mime_type": "application/json"},
+            generation_config={"response_mime_type": "application/json", "temperature": 0},
         )
 
         with open(self.input_file, "r", encoding="utf-8") as original_file, open(self.output_file, "w", encoding="utf-8") as translated_file:
@@ -67,81 +67,53 @@ class GeminiSRTTranslator:
             original_subtitle = list(srt.parse(original_text))
             translated_subtitle = original_subtitle.copy()
 
-            chat = model.start_chat()
-
             i = 0
             total = len(original_subtitle)
             batch = {}
 
             print(f"Starting translation of {total} lines...")
+
             while i < total:
                 if len(batch) < self.batch_size:
                     batch[str(i)] = original_subtitle[i].content
                     i += 1
                     continue
                 else:
-                    try:
-                        response = chat.send_message(json.dumps(batch))
-                        try:
-                            translated_lines = json.loads(response.text)
-                            if len(translated_lines) != len(batch):
-                                raise Exception("Gemini has returned a different number of lines than the original, trying again...")
-                            for x in translated_lines:
-                                if x not in batch:
-                                    raise Exception("Gemini has returned different indices than the original, trying again...")
-                        except Exception as e:
-                            print(e)
-                            chat.rewind()
-                            continue
-                        for x in translated_lines:
-                            translated_subtitle[int(x)].content = translated_lines[x]
-                        batch = {}
-
-                        print(f"Translated {i}/{total}")
-                    except Exception as e:
-                        e = str(e)
-                        if "block" in e:
-                            print(e)
-                            batch = {}
-                            break
-                        elif "quota" in e:
-                            print("Quota exceeded, waiting 1 minute...")
-                            time.sleep(60)
-                            continue
-                        else:
-                            print(e)
-                            continue
-            while len(batch) > 0:
-                try:
-                    response = chat.send_message(json.dumps(batch))
-                    try:
-                        translated_lines = json.loads(response.text)
-                        if len(translated_lines) != len(batch):
-                            raise Exception("Gemini has returned a different number of lines than the original, trying again...")
-                        for x in translated_lines:
-                            if x not in batch:
-                                raise Exception("Gemini has returned different indices than the original, trying again...")
-                    except Exception as e:
-                        print(e)
-                        chat.rewind()
-                        continue
-                    for x in translated_lines:
-                        translated_subtitle[int(x)].content = translated_lines[x]
-                    batch = {}
-
+                    self._process_batch(model, batch, translated_subtitle)
                     print(f"Translated {i}/{total}")
-                except Exception as e:
-                        e = str(e)
-                        if "block" in e:
-                            print(e)
-                            batch = {}
-                            break
-                        elif "quota" in e:
-                            print("Quota exceeded, waiting 1 minute...")
-                            time.sleep(60)
-                            continue
-                        else:
-                            print(e)
-                            continue
+
+            while len(batch) > 0:
+                self._process_batch(model, batch, translated_subtitle)
+                print(f"Translated {i}/{total}")
 
             translated_file.write(srt.compose(translated_subtitle))
+
+    def _process_batch(self, model, batch, translated_subtitle):
+        """
+        Processes a batch of subtitles.
+        """
+        try:
+            response = model.generate_content(json.dumps(batch))
+            try:
+                translated_lines = json.loads(response.text)
+                if len(translated_lines) != len(batch):
+                    raise Exception("Gemini has returned a different number of lines than the original, trying again...")
+                for x in translated_lines:
+                    if x not in batch:
+                        raise Exception("Gemini has returned different indices than the original, trying again...")
+            except Exception as e:
+                print(e)
+                return
+            for x in translated_lines:
+                translated_subtitle[int(x)].content = translated_lines[x]
+            batch.clear()
+        except Exception as e:
+            e = str(e)
+            if "block" in e:
+                print(e)
+                batch.clear()
+            elif "quota" in e:
+                print("Quota exceeded, waiting 1 minute...")
+                time.sleep(60)
+            else:
+                print(e)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gemini-srt-translator",
-    version="1.1.3",
+    version="1.1.4",
     packages=find_packages(),
     install_requires=[
         "google-generativeai==0.8.3",
