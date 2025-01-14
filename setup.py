fe1fbf71d86e16df67f40cd2e745cc7ada25b528
Amoz <zeegfi2@gmail.com>
2025-01-14 16:38:15 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="gemini-srt-translator",
-    version="1.3.0",
+    version="1.4.0",
     packages=find_packages(),
     install_requires=[
         "google-generativeai==0.8.3",
         "srt==3.5.3",
+        "PyQt6>=6.0.0",
     ],
+    entry_points={
+        'console_scripts': [
+            'gemini-srt-translator=gemini_srt_translator:translate',
+            'gemini-srt-translator-gui=subtitle-translator-gui:main'
+        ],
+    },
     author="Matheus Castro",
     description="A tool to translate subtitles using Google Generative AI",
     long_description=open("README.md").read(),
@@ -16,6 +23,7 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
@@ -25,6 +33,7 @@ setup(
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Text Processing :: Linguistic",
+        "Environment :: X11 Applications :: Qt",
     ],
     python_requires='>=3.9',
 )
